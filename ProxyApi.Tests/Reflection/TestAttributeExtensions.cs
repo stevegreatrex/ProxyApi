3031b2180d5a14756e34973f90f247b3c03d9c9f
Steve Greatrex <stephen.greatrex@googlemail.com>
2012-11-01 20:08:49 +0000

    Productised attributeextensions

---
+++ ProxyApi.Tests/Reflection/TestAttributeExtensions.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.ComponentModel;
+using System.Web.Http;
+using System.Reflection;
+using ProxyApi.Reflection;
+
+namespace ProxyApi.Tests.Reflection
+{
+	/// <summary>
+	/// Tests the functionality of the <see cref="AttributeExtensions"/> class.
+	/// </summary>
+	[TestClass]
+	public class TestAttributeExtensions
+	{
+		private MethodInfo _withDescriptionAttribute;
+		private MethodInfo _withBrowseableAttribute;
+		private ParameterInfo _withFromBodyAttribute;
+		private ParameterInfo _withoutFromBodyAttribute;
+
+		[TestInitialize]
+		public void GetReflectedInfo()
+		{
+			var sample = typeof(Sample);
+			_withDescriptionAttribute = sample.GetMethod("WithDescriptionAttribute");
+			_withBrowseableAttribute = sample.GetMethod("WithBrowseableAttribute");
+
+			var parameters = sample.GetMethod("Parameters").GetParameters();
+			_withFromBodyAttribute = parameters[0];
+			_withoutFromBodyAttribute = parameters[1];
+		}
+
+		/// <summary>
+		/// Ensures that GetCustomAttribute throws an exception when called on a null member
+		/// </summary>
+		[TestMethod]
+		[ExpectedException(typeof(ArgumentNullException))]
+		public void GetCustomAttribute_Throws_On_Null_Method()
+		{
+			MethodInfo target = null;
+			target.GetCustomAttribute<System.ComponentModel.DescriptionAttribute>();
+		}
+
+		/// <summary>
+		/// Ensures that GetCustomAttribute returns the expected attribute when present
+		/// </summary>
+		[TestMethod]
+		public void GetCustomAttribute_Returns_Attribute_When_Present_On_Method()
+		{
+			var description = _withDescriptionAttribute.GetCustomAttribute<System.ComponentModel.DescriptionAttribute>();
+			var blank		= _withBrowseableAttribute.GetCustomAttribute<System.ComponentModel.DescriptionAttribute>();
+
+			Assert.IsNotNull(description);
+			Assert.IsNull(blank);
+		}
+
+		/// <summary>
+		/// Ensures that GetCustomAttribute throws an exception when called on a null member
+		/// </summary>
+		[TestMethod]
+		[ExpectedException(typeof(ArgumentNullException))]
+		public void GetCustomAttribute_Throws_On_Null_Parameter()
+		{
+			ParameterInfo target = null;
+			target.GetCustomAttribute<FromBodyAttribute>();
+		}
+
+		/// <summary>
+		/// Ensures that GetCustomAttribute returns the expected attribute when present
+		/// </summary>
+		[TestMethod]
+		public void GetCustomAttribute_Returns_Attribute_When_Present_On_Parameter()
+		{
+			var fromBody	= _withFromBodyAttribute.GetCustomAttribute<FromBodyAttribute>();
+			var blank		= _withoutFromBodyAttribute.GetCustomAttribute<FromBodyAttribute>();
+
+			Assert.IsNotNull(fromBody);
+			Assert.IsNull(blank);
+		}
+
+		/// <summary>
+		/// Ensures that HasAttribute throws an exception when called on a null member
+		/// </summary>
+		[TestMethod]
+		[ExpectedException(typeof(ArgumentNullException))]
+		public void HasAttribute_Throws_On_Null_Method()
+		{
+			MethodInfo target = null;
+			target.HasAttribute<System.ComponentModel.DescriptionAttribute>();
+		}
+
+		/// <summary>
+		/// Ensures that HasAttribute returns the expected attribute when present
+		/// </summary>
+		[TestMethod]
+		public void HasAttribute_Returns_Attribute_When_Present_On_Method()
+		{
+			var description = _withDescriptionAttribute.HasAttribute<System.ComponentModel.DescriptionAttribute>();
+			var blank		= _withBrowseableAttribute.HasAttribute<System.ComponentModel.DescriptionAttribute>();
+
+			Assert.IsTrue(description);
+			Assert.IsFalse(blank);
+		}
+
+		/// <summary>
+		/// Ensures that HasAttribute throws an exception when called on a null member
+		/// </summary>
+		[TestMethod]
+		[ExpectedException(typeof(ArgumentNullException))]
+		public void HasAttribute_Throws_On_Null_Parameter()
+		{
+			ParameterInfo target = null;
+			target.HasAttribute<FromBodyAttribute>();
+		}
+
+		/// <summary>
+		/// Ensures that HasAttribute returns the expected attribute when present
+		/// </summary>
+		[TestMethod]
+		public void HasAttribute_Returns_Attribute_When_Present_On_Parameter()
+		{
+			var fromBody	= _withFromBodyAttribute.HasAttribute<FromBodyAttribute>();
+			var blank		= _withoutFromBodyAttribute.HasAttribute<FromBodyAttribute>();
+
+			Assert.IsTrue(fromBody);
+			Assert.IsFalse(blank);
+		}
+
+		class Sample
+		{
+			[System.ComponentModel.Description]
+			public void WithDescriptionAttribute()
+			{}
+
+			[System.ComponentModel.Browsable(false)]
+			public void WithBrowseableAttribute()
+			{}
+
+			public void Parameters([FromBody]object withFromBody, object withoutFromBody)
+			{}
+		}
+	}
+}
--- ProxyApi/Reflection/AttributeExtensions.cs
+++ ProxyApi/Reflection/AttributeExtensions.cs
@@ -6,29 +6,72 @@ using System.Text;
 
 namespace ProxyApi.Reflection
 {
+	/// <summary>
+	/// Extension methods to help with checking on custom attributes.
+	/// </summary>
 	public static class AttributeExtensions
 	{
+		/// <summary>
+		/// Gets the custom attribute instance of type <typeparamref name="TAttribute"/> on
+		/// <paramref name="method"/>, if one exists.
+		/// </summary>
+		/// <typeparam name="TAttribute">The type of the attribute for which to look.</typeparam>
+		/// <param name="method">The method on which to check.</param>
+		/// <param name="inherit">if set to <c>true</c>, inherited attributes will be considered.</param>
+		/// <returns>The matching attribute, or <c>null</c></returns>
 		public static TAttribute GetCustomAttribute<TAttribute>(this MethodInfo method, bool inherit = true)
 			where TAttribute : Attribute
 		{
+			if (method == null) throw new ArgumentNullException("method");
+
 			return method.GetCustomAttributes(inherit).OfType<TAttribute>().FirstOrDefault();
 		}
 
+		/// <summary>
+		/// Gets the custom attribute instance of type <typeparamref name="TAttribute"/> on
+		/// <paramref name="parameter"/>, if one exists.
+		/// </summary>
+		/// <typeparam name="TAttribute">The type of the attribute for which to look.</typeparam>
+		/// <param name="parameter">The method on which to check.</param>
+		/// <param name="inherit">if set to <c>true</c>, inherited attributes will be considered.</param>
+		/// <returns>The matching attribute, or <c>null</c></returns>
 		public static TAttribute GetCustomAttribute<TAttribute>(this ParameterInfo parameter, bool inherit = true)
 			where TAttribute : Attribute
 		{
+			if (parameter == null) throw new ArgumentNullException("parameter");
+
 			return parameter.GetCustomAttributes(inherit).OfType<TAttribute>().FirstOrDefault();
 		}
 		
+		/// <summary>
+		/// Returns true if an instance of type <typeparamref name="TAttribute"/> exists on
+		/// <paramref name="method"/>.
+		/// </summary>
+		/// <typeparam name="TAttribute">The type of the attribute for which to look.</typeparam>
+		/// <param name="parameter">The method on which to check.</param>
+		/// <param name="inherit">if set to <c>true</c>, inherited attributes will be considered.</param>
+		/// <returns><c>true</c> if a matching attribute exists</returns>
 		public static bool HasAttribute<TAttribute>(this MethodInfo method, bool inherit = true)
 			where TAttribute : Attribute
 		{
+			if (method == null) throw new ArgumentNullException("method");
+
 			return method.GetCustomAttribute<TAttribute>(inherit) != null;
 		}
 
+		/// <summary>
+		/// Returns true if an instance of type <typeparamref name="TAttribute"/> exists on
+		/// <paramref name="method"/>.
+		/// </summary>
+		/// <typeparam name="TAttribute">The type of the attribute for which to look.</typeparam>
+		/// <param name="parameter">The method on which to check.</param>
+		/// <param name="inherit">if set to <c>true</c>, inherited attributes will be considered.</param>
+		/// <returns><c>true</c> if a matching attribute exists</returns>
 		public static bool HasAttribute<TAttribute>(this ParameterInfo parameter, bool inherit = true)
 			where TAttribute : Attribute
 		{
+			if (parameter == null) throw new ArgumentNullException("parameter");
+
 			return parameter.GetCustomAttribute<TAttribute>(inherit) != null;
 		}
 	}
