eb48349238c9444af0527cfebb1b9f3c275ebbba
Steve Greatrex <stephen.greatrex@googlemail.com>
2012-11-01 22:21:49 +0000

    Productised path utility, but had to exclude from code coverage

--- ProxyApi/IPathUtility.cs
+++ ProxyApi/IPathUtility.cs
@@ -6,8 +6,16 @@ using System.Threading.Tasks;
 
 namespace ProxyApi
 {
+	/// <summary>
+	/// An interface for classes that provide utility methods around virtual paths.
+	/// </summary>
 	public interface IPathUtility
 	{
+		/// <summary>
+		/// Converts <paramref name="path"/> to an absolute path.
+		/// </summary>
+		/// <param name="path">The virtual path.</param>
+		/// <returns>The absolute equivalent of the virtual path.</returns>
 		string ToAbsolute(string path);
 	}
 }
--- ProxyApi/PathUtility.cs
+++ ProxyApi/PathUtility.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.Composition;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,9 +9,20 @@ using System.Web;
 
 namespace ProxyApi
 {
+	/// <summary>
+	/// A wrapper class around the <see cref="VirtualPathUtility"/> class.
+	/// </summary>
 	[Export(typeof(IPathUtility))]
+	[ExcludeFromCodeCoverage]
 	public class PathUtility : IPathUtility
 	{
+		/// <summary>
+		/// Converts <paramref name="path" /> to an absolute path.
+		/// </summary>
+		/// <param name="path">The virtual path.</param>
+		/// <returns>
+		/// The absolute equivalent of the virtual path.
+		/// </returns>
 		public string ToAbsolute(string path)
 		{
 			return VirtualPathUtility.ToAbsolute(path);
