fa7e331ca614fff536566f549269289d4ba9187c
Steve Greatrex <stephen.greatrex@googlemail.com>
2012-11-06 19:25:07 +0000

    Added inclusion/exclusion attributes

---
+++ ProxyApi.Tests/TestProxyInclusionRuleAttribute.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ProxyApi.Tests
+{
+	/// <summary>
+	/// Tests the functionality of the <see cref="ProxyInclusionRuleAttribute"/> class.
+	/// </summary>
+	[TestClass]
+	public class TestProxyInclusionRuleAttribute
+	{
+		/// <summary>
+		/// Ensures that Constructor sets InclusionRule
+		/// </summary>
+		[TestMethod]
+		public void Constructor_Sets_InclusionRule()
+		{
+			var testSubject = new ProxyInclusionAttribute(InclusionRule.ExcludeAll);
+			Assert.AreEqual(InclusionRule.ExcludeAll, testSubject.InclusionRule);
+		}
+
+		/// <summary>
+		/// Ensures that ProxyIncludeAttribute sets IncludeAll
+		/// </summary>
+		[TestMethod]
+		public void ProxyIncludeAttribute_Sets_IncludeAll()
+		{
+			var testSubject = new ProxyIncludeAttribute();
+			Assert.AreEqual(InclusionRule.IncludeAll, testSubject.InclusionRule);
+		}
+
+		/// <summary>
+		/// Ensures that ProxyExcludeAttribute sets ExcludeAll
+		/// </summary>
+		[TestMethod]
+		public void ProxyExcludeAttribute_Sets_ExcludeAll()
+		{
+			var testSubject = new ProxyExcludeAttribute();
+			Assert.AreEqual(InclusionRule.ExcludeAll, testSubject.InclusionRule);
+		}
+	}
+}
---
+++ ProxyApi/ProxyInclusionAttribute.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ProxyApi
+{
+	/// <summary>
+	/// An attribute for specifying a non-default inclusion rule.
+	/// </summary>
+	[AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
+	public class ProxyInclusionAttribute : Attribute
+	{
+		/// <summary>
+		/// Initializes a new instance of the <see cref="ProxyInclusionAttribute" /> class.
+		/// </summary>
+		public ProxyInclusionAttribute(InclusionRule inclusionRule)
+		{
+			this.InclusionRule = inclusionRule;
+		}
+
+		/// <summary>
+		/// Gets the inclusion rule.
+		/// </summary>
+		/// <value>
+		/// The inclusion rule.
+		/// </value>
+		public InclusionRule InclusionRule { get; private set; }
+	}
+
+	/// <summary>
+	/// An attribute for explicitly specifying that an element should be included
+	/// in the proxy.
+	/// </summary>
+	public class ProxyIncludeAttribute : ProxyInclusionAttribute
+	{
+		/// <summary>
+		/// Initializes a new instance of the <see cref="ProxyIncludeAttribute" /> class.
+		/// </summary>
+		public ProxyIncludeAttribute()
+			: base(InclusionRule.IncludeAll)
+		{}
+	}
+
+	/// <summary>
+	/// An attribute for explicitly specifying that an element should be excluded
+	/// from the proxy.
+	/// </summary>
+	public class ProxyExcludeAttribute : ProxyInclusionAttribute
+	{
+		/// <summary>
+		/// Initializes a new instance of the <see cref="ProxyExcludeAttribute" /> class.
+		/// </summary>
+		public ProxyExcludeAttribute()
+			: base(InclusionRule.ExcludeAll)
+		{}
+	}
+}
