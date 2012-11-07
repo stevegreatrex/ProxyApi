f3c20cd85b5e6689312e7647b39f3b88776e1c53
Steve Greatrex <stephen.greatrex@googlemail.com>
2012-11-07 18:59:58 +0000

    Moved nuget resources around and added template for intellisense

---
+++ nuget/Tools/install.ps1
@@ -0,0 +1,6 @@
+param($installPath, $toolsPath, $package, $project)
+
+$path = [System.IO.Path]
+$referencesFile = $path::Combine($path::GetDirectoryName($project.FileName), "Scripts\_references.js")
+
+Add-Content $referencesFile "`r`n/// <reference path=""ProxyApi.proxies.js"" />"
