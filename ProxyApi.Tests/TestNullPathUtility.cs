237eb2a8e95b0883d0767998253db1d79223b2cc
Steve Greatrex <stephen.greatrex@googlemail.com>
2012-11-07 19:08:56 +0000

    Added NullPathUtility and updated nuget package to install .tt

---
+++ ProxyApi.Tests/TestNullPathUtility.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ProxyApi.Tests
+{
+	[TestClass]
+	public class TestNullPathUtility
+	{
+		/// <summary>
+		/// Ensures that ToAbsolute returns an empty string
+		/// </summary>
+		[TestMethod]
+		public void ToAbsolute_Returns_Empty_String()
+		{
+			Assert.AreEqual(string.Empty, new NullPathUtility().ToAbsolute(null));
+		}
+
+		/// <summary>
+		/// Ensures that GetVirtualPath returns an empty string
+		/// </summary>
+		[TestMethod]
+		public void GetVirtualPath_Returns_Empty_String()
+		{
+			Assert.AreEqual(string.Empty, new NullPathUtility().GetVirtualPath(null));
+		}
+	}
+}
---
+++ ProxyApi/NullPathUtility.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ProxyApi
+{
+	/// <summary>
+	/// A null-pattern implementation of <see cref="IPathUtility"/>.
+	/// </summary>
+	public class NullPathUtility : IPathUtility
+	{
+		/// <summary>
+		/// Converts <paramref name="path" /> to an absolute path.
+		/// </summary>
+		/// <param name="path">The virtual path.</param>
+		/// <returns>
+		/// The absolute equivalent of the virtual path.
+		/// </returns>
+		public string ToAbsolute(string path)
+		{
+			return string.Empty;
+		}
+
+		/// <summary>
+		/// Gets the virtual path for the route specified by the values in <paramref name="routeValues" />.
+		/// </summary>
+		/// <param name="routeValues">The route values dictionary.</param>
+		/// <returns>
+		/// The virtual path for the route.
+		/// </returns>
+		public string GetVirtualPath(System.Web.Routing.RouteValueDictionary routeValues)
+		{
+			return string.Empty;
+		}
+	}
+}
