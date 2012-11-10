8d97b1adbf153388e411f8922e377e58eb93ecfc
Steve Greatrex <stephen.greatrex@googlemail.com>
2012-11-10 22:36:19 +0000

    NuSpec for .Intellisense
    Added a nuspec file for intellisense package

--- ProxyApi/build-nuget.bat
+++ ProxyApi/build-nuget.bat
@@ -1,1 +1,2 @@
 ..\.nuget\nuget pack ProxyApi.csproj -OutputDirectory ..\Nuget\Packages -Prop Configuration=Release
+..\.nuget\nuget pack ProxyApi.Intellisense.nuspec -OutputDirectory ..\Nuget\Packages
