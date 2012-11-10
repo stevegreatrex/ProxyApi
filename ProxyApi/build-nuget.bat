64c5dfba7d168952c340756ace28ecedac46c7e9
Steve Greatrex <stephen.greatrex@googlemail.com>
2012-11-10 23:18:08 +0000

    Added package building to build process

--- ProxyApi/build-nuget.bat
+++ ProxyApi/build-nuget.bat
@@ -1,2 +1,12 @@
-..\.nuget\nuget pack ProxyApi.csproj -OutputDirectory ..\Nuget\Packages -Prop Configuration=Release
-..\.nuget\nuget pack ProxyApi.Intellisense.nuspec -OutputDirectory ..\Nuget\Packages
+@echo off
+set path=.
+if not [%1]==[] set path=%1
+
+echo Creating ProxyApi Package
+%path%\..\.nuget\nuget pack %path%\ProxyApi.csproj -OutputDirectory %path%\..\Nuget\Packages -Prop Configuration=Release
+
+if errorlevel 1 echo Error creating ProxyApi Package
+
+echo Creating ProxyApi.Intellisense Package
+%path%\..\.nuget\nuget pack %path%\ProxyApi.Intellisense.nuspec -OutputDirectory %path%\..\Nuget\Packages
+if errorlevel 1 echo Error creating ProxyApi.Intellisense Package
