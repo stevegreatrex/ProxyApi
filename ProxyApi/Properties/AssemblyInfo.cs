e797b898ca5f97035531db671798aa0148306dd2
Steve Greatrex <stephen.greatrex@googlemail.com>
2012-11-10 22:00:28 +0000

    NuGet build for ProxyApi
    Added a build script to create the nuget package for the main ProxyApi
    project

--- ProxyApi/Properties/AssemblyInfo.cs
+++ ProxyApi/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("ProxyApi")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A library to auto-generate JavaScript proxy objects from WebAPI and MVC controllers.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Steve Greatrex")]
 [assembly: AssemblyProduct("ProxyApi")]
 [assembly: AssemblyCopyright("Copyright ©  2012")]
 [assembly: AssemblyTrademark("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.1.0.*")]
-[assembly: AssemblyFileVersion("0.1")]
+[assembly: AssemblyVersion("0.1.11.*")]
+[assembly: AssemblyFileVersion("0.1.11")]
