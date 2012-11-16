6ce93be86e4886601da1aed522abf34ffe378839
Steve Greatrex <greatrex696@hotmail.com>
2012-11-16 14:05:12 +0000

    Fixed encoding issue in powershell

--- nuget/Tools/install.ps1
+++ nuget/Tools/install.ps1
@@ -3,4 +3,4 @@ param($installPath, $toolsPath, $package, $project)
 $path = [System.IO.Path]
 $referencesFile = $path::Combine($path::GetDirectoryName($project.FileName), "Scripts\_references.js")
 
-Add-Content $referencesFile "`r`n/// <reference path=""proxyapi.intellisense.js"" />"
+Add-Content -Encoding ASCII $referencesFile "/// <reference path=""proxyapi.intellisense.js"" />"
