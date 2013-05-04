0abc924432ea571f0cdf2423eadc556995bb238f
Steve Greatrex <stephen.greatrex@googlemail.com>
2013-05-04 09:35:53 +0100

    Excluded abstract classes from generation

---
+++ ProxyApi.Tests/SampleAbstractApiController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Http;
+
+namespace ProxyApi.Tests
+{
+	/// <summary>
+	/// This type should be excluded because it is abstract
+	/// </summary>
+	public abstract class SampleAbstractApiController : ApiController
+	{
+	}
+}
