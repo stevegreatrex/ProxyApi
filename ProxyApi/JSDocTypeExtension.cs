6208b60760c812e11b30f0cabf1d16e6ffe9f6e7
Oren <oren.ferrari@renaissance.com>
2015-08-03 20:43:51 -0700

    Exposed JSDocs for web api actions. This is the first attempt and has been tested using WebStorm as a JSDoc IDE. It could be expanded much more as well as adding TypeScript defs.

---
+++ ProxyApi/JSDocTypeExtension.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ProxyApi
+{
+	public static class JSDocTypeExtension
+	{
+		public static string JsDocTypeString(this Type type)
+		{
+			if (type == typeof(String))
+				return "String";
+
+			if (type.IsArray)
+				return "Array";
+
+			if (type.IsPrimitive)
+			{
+				if (type == typeof(bool))
+					return "Boolean";
+
+				if (type == typeof(int)
+				|| type == typeof(double)
+				|| type == typeof(float)
+				|| type == typeof(decimal)
+				|| type == typeof(uint)
+				|| type == typeof(short)
+				|| type == typeof(ushort)
+				|| type == typeof(long)
+				|| type == typeof(ulong)
+				|| type == typeof(bool))
+					return "Number";
+
+			}
+
+			return "Object";
+		}
+	}
+}
